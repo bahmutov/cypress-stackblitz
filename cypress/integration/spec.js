14c533cc8ad17e850f33118dd5f4bc6ff3551664
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-06-04 16:27:26 -0400

    add intercept

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,5 +1,9 @@
 /// <reference types="cypress" />
 
 it('works', () => {
+  cy.intercept('https://jsonplaceholder.cypress.io/users', {
+    fixture: 'users.json'
+  }).as('users')
   cy.visit('/')
+  cy.wait('@users', {timeout: 75000})
 })
